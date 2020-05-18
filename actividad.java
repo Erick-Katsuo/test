ca92a5eec99005343a8d7bfe1c65f2388d060bf1
Katsuo <49467285+Erick-Katsuo@users.noreply.github.com>
2020-05-18 18:18:15 -0300

    Update actividad.java

--- actividad.java
+++ actividad.java
@@ -1,26 +1,27 @@
-
-package actividad2;
+package appmaxi;
 
 import java.awt.*;
 import javax.swing.*;
 import javax.swing.event.*;
 
-public class Actividad2 extends JFrame{
+public class Actividad2 extends JFrame {
+
     //Declaro Objetos.
-    JSlider velocidad,distancia;
-    JLabel Labelvelocidad,Labeldistancia,Labeltiempo;
+    JSlider velocidad, distancia;
+    JLabel Labelvelocidad, Labeldistancia, Labeltiempo;
+
     //Constructor.
-    public Actividad2(){
+    public Actividad2() {
         super("Tiempo");
-        
-        setSize(1050,800);
+
+        setSize(1050, 800);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
-        setResizable(false);
-        
-         //FlowLayout
-        setLayout(new GridLayout(1,2)); 
-    
+        setResizable(true);
+
+        //FlowLayout
+        setLayout(new GridLayout(1, 2));
+
         //Objetos.
         setLookAndFeel();
         //Velocidad
@@ -28,7 +29,7 @@ public class Actividad2 extends JFrame{
         velocidad.setMajorTickSpacing(25);
         velocidad.setMinorTickSpacing(5);
         velocidad.setPaintTicks(true);
-        velocidad.setFont(new Font("Serif", Font.ITALIC,12));
+        velocidad.setFont(new Font("Serif", Font.ITALIC, 12));
         velocidad.setPaintLabels(true);
         //Distancia
         distancia = new JSlider(JSlider.VERTICAL, 0, 400, 5);
@@ -36,32 +37,61 @@ public class Actividad2 extends JFrame{
         distancia.setMinorTickSpacing(5);
         distancia.setPaintTicks(true);
         distancia.setPaintLabels(true);
-        distancia.setFont(new Font("Serif", Font.ITALIC,12));
+        distancia.setFont(new Font("Serif", Font.ITALIC, 12));
         //Etiqueta
         Labelvelocidad = new JLabel("Velocidad: 0");
         Labeldistancia = new JLabel("Distancia: 0");
         Labeltiempo = new JLabel("Tiempo: 0");
         //Muestra la Ventana.
-        setVisible(true); 
+        setVisible(true);
         add(velocidad);
         add(distancia);
         add(Labelvelocidad);
         add(Labeldistancia);
         add(Labeltiempo);
+        
+        //uso el objeto velocidad para obtner el evento del slider
+        velocidad.addChangeListener(new javax.swing.event.ChangeListener() {
+            public void stateChanged(javax.swing.event.ChangeEvent evt) {
+                jSlider1StateChanged(evt);
+            }
+        });
+        
     }
- private void setLookAndFeel(){
+    
+    
+    
+    private void setLookAndFeel() {
         try {
             UIManager.setLookAndFeel(
-                "com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
-        }catch(Exception e){ 
-        
-            //No hacer nada
-           
-        }
-    }   
- 
+                    "com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+        } catch (Exception e) {
 
- public static void main(String[] args) {
+            //No hacer nada
+        }
+    }
+    
+    private void jSlider1StateChanged(javax.swing.event.ChangeEvent evt) {                                      
+        // TODO add your handling code here:
+        //calcularVelocidad();
+        System.out.println("FUNCIONA");
+    } 
+    
+    public void calcularVelocidad(){
+        // x = x_inicial + velocidad * t
+        // x / v = t
+        int resultadoTiempo = 0;
+        
+        resultadoTiempo = distancia.getValue() / velocidad.getValue();
+        
+        //Labeltiempo.setText( String.valueOf(resultadoTiempo));
+        
+        Labeltiempo.setText("Tiempo"+ resultadoTiempo);
+        
+    }
+    
+    
+    public static void main(String[] args) {
         //llamara al contructor
         new Actividad2();
     }
